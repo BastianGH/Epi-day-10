f326e2f0733f66cd37f2390f4533e3fddeeeb4b8
BastianGH <bastianpussey16@gmail.com>
2022-09-21 20:05:18 +0200

    Commencement du front à l'aide de VUE CLI

---
+++ step02/Front/my-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Header_comp msg="Job Seeker" adress="../public/index.html"/> 
+  <Main_comp msg="Find your dream's job"/>
+  <Footer_comp msg=""/> 
+</template>
+
+<script>
+import Header_comp from './components/Header.vue';
+import Main_comp from './components/Main.vue';
+import Footer_comp from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header_comp,
+    Main_comp,
+    Footer_comp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+a {
+  text-decoration: none;    
+  color: black;
+}
+</style>
---
+++ step02/Front/my-project/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <a href="{{ adress }}">
+            <h1>{{ msg }}</h1>   
+        </a>         
+    </header>    
+</template>
+  
+<script>
+export default {
+    name: 'Header_comp',
+    props: {
+    msg: String,
+    adress: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+    background-image: linear-gradient(0.49turn,grey 15%, red 45%, white 50%);
+    display: flex;
+    justify-content: center;
+}
+h1{
+    text-align: center;
+    font-style: italic;
+    text-shadow: blue 4px 1px 3px ;
+}
+</style>
+    
